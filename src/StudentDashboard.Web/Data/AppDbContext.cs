162b1aec1eb7543069fcbb107a0c65d1b32b9b3f
SufyanAmjad804 <247237@students.au.edu.pk>
2026-05-16 22:04:35 +0500

    commit 6

--- src/StudentDashboard.Web/Data/AppDbContext.cs
+++ src/StudentDashboard.Web/Data/AppDbContext.cs
@@ -7,5 +7,8 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-    }
+        public DbSet<Student> Students => Set<Student>();
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+}
 }
